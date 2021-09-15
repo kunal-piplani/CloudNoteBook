5c581beee1be498f77cd7dfd3814d7b4d9f676c2
kunal piplani <kunal.piplani12@gmail.com>
2021-09-15 11:50:28 +0530

    V-49-CloudNoteBOOK

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,9 @@ const express =require('express');
 const router =express.Router();
 const User  =require('../models/User');
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET="ItsKunalPiplani$1234";
 //create user using post method "/api/auth/",doest require auth 
 router.post('/CreateUser',body('email','Enter a Valid Mail').isEmail(),body('name','Enter Valid Name').isLength({ min: 5 }),body('password','Password atLest 5 Character ').isLength({ min: 5 }),
 
@@ -25,16 +27,30 @@ async(req,res)=>{
 res.status(400).json({error:'Sorry user exist with that email'});
 
  }
+ const salt =await bcrypt.genSalt(10);
+const secPass=await bcrypt.hash(req.body.password,salt);
   user =await User.create({
     name: req.body.name,
-    password: req.body.password,
+    password: secPass,
     email: req.body.email,
   })
   
 //   then(user => res.json(user)).catch(err=>{console.log(err) 
 //     res.json({error: 'please ente unique Value ' ,
 //     message: err.message })});
-res.json({nice:'entered'})
+const data = {
+
+user:{
+id:user.id
+
+
+}
+
+}
+const authToken = jwt.sign(data,JWT_SECRET);
+res.json({authToken});
+
+
 }catch{
 console.error(error.message);
 res.sendStatus(500).send("Some error ocuured ")
