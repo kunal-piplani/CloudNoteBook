e53334108735df56bcc9779f46139ddb6877fa82
kunal piplani <kunal.piplani12@gmail.com>
2021-09-16 18:48:16 +0530

    V 62

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Navbar from "./components/Navbar";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NoteState from "./context/note/NoteState";
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
       <NoteState>
         <Router>
           <Navbar />
-
+          <Alert/>
           <div className="container">
             <Switch>
               <Route exact path="/">
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Noteitem = (props) => {
-    const {note} =props;
-    return (
-        <div className ="col-md-3">
-      
-            <div className="card my-3" >
-             <div className="card-body">
-    <h5 className="card-title">{note.title}</h5>
-    <p className="card-text">{note.description}  </p>
-  </div>
-</div>
-        </div>
-    )
-}
+  const { note } = props;
+  return (
+    <div className="col-md-3">
+      <div className="card my-3">
+        <div className="card-body">
+          <div className="d-flex align-items-center">
+            <h5 className="card-title">{note.title}</h5>
 
-export default Noteitem
+            <i className="fas fa-minus-circle mx-2"></i>
+            <i className="fas fa-pen-square mx-2"></i>
+          </div>
+          <p className="card-text">{note.description} </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Noteitem;
