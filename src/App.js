46b4b72b5e5682b749dc746f91a92560e322cccb
kunal piplani <kunal.piplani12@gmail.com>
2021-09-16 15:09:04 +0530

    V 57

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Aboutus from "./components/Aboutus";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+
+        <Switch>
+          <Route exact="/">
+          <Home />
+
+          </Route>
+          <Route exact="/about">
+            <Aboutus/>
+          </Route>
+          
+        </Switch>
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/components/Aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Aboutus() {
+    return (
+        <div>
+            This is about us 
+        </div>
+    )
+}
+
+export default Aboutus
